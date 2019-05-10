39992ba55c2f8b253f48b594086f63f65efc31d0
Enrico Bonaldo <asclepix@yahoo.it>
2019-05-10 16:23:20 +0200

    Aggiornato converter da xccov a cobertura
    Modificato per ottenere miglior dettaglio delle linee coperte da test.

--- xccov_converter.swift
+++ xccov_converter.swift
@@ -15,7 +15,7 @@ extension String {
                 return true
             }
         }
-        
+
         return false
     }
 }
@@ -57,12 +57,12 @@ struct CoverageReport: Codable {
     let coveredLines: Int
 }
 
-func generateCoberturaReport(from coverageReport: CoverageReport, targetsToInclude: [String], packagesToExclude: [String], workingDirectory: String) -> String {
-    
+func generateCoberturaReport(from coverageReport: CoverageReport, targetsToInclude: [String], packagesToExclude: [String], workingDirectory: String, covPath: String) -> String {
+
     let dtd = try! XMLDTD(contentsOf: URL(string: "http://cobertura.sourceforge.net/xml/coverage-04.dtd")!)
     dtd.name = "coverage"
     dtd.systemID = "http://cobertura.sourceforge.net/xml/coverage-04.dtd"
-    
+
     let rootElement = XMLElement(name: "coverage")
     rootElement.addAttribute(XMLNode.attribute(withName: "line-rate", stringValue: "\(coverageReport.lineCoverage)") as! XMLNode)
     rootElement.addAttribute(XMLNode.attribute(withName: "branch-rate", stringValue: "1.0") as! XMLNode)
@@ -73,51 +73,51 @@ func generateCoberturaReport(from coverageReport: CoverageReport, targetsToInclu
     rootElement.addAttribute(XMLNode.attribute(withName: "complexity", stringValue: "0.0") as! XMLNode)
     rootElement.addAttribute(XMLNode.attribute(withName: "branches-valid", stringValue: "1.0") as! XMLNode)
     rootElement.addAttribute(XMLNode.attribute(withName: "branches-covered", stringValue: "1.0") as! XMLNode)
-    
+
     let doc = XMLDocument(rootElement: rootElement)
     doc.version = "1.0"
     doc.dtd = dtd
     doc.documentContentKind = .xml
-    
+
     let sourceElement = XMLElement(name: "sources")
     rootElement.addChild(sourceElement)
     sourceElement.addChild(XMLElement(name: "source", stringValue: workingDirectory))
-    
+
     let packagesElement = XMLElement(name: "packages")
     rootElement.addChild(packagesElement)
-    
+
     var allFiles = [FileCoverageReport]()
     for targetCoverageReport in coverageReport.targets {
 
         guard targetsToInclude.isEmpty || targetsToInclude.contains(targetCoverageReport.name) else {
             continue
         }
-        
+
         // Filter out files by package
         let targetFiles = targetCoverageReport.files.filter { !$0.path.contains(elementOfArray: packagesToExclude) }
         allFiles.append(contentsOf: targetFiles)
     }
-    
+
     // Sort files to avoid duplicated packages
     allFiles = allFiles.sorted(by: { $0.path > $1.path })
-    
+
     var currentPackage = ""
     var currentPackageElement: XMLElement!
     var isNewPackage = false
-    
+
     for fileCoverageReport in allFiles {
         // Define file path relative to source!
         let filePath = fileCoverageReport.path.replacingOccurrences(of: workingDirectory + "/", with: "")
         let pathComponents = filePath.split(separator: "/")
         let packageName = pathComponents[0..<pathComponents.count - 1].joined(separator: ".")
-        
+
         isNewPackage = currentPackage != packageName
-        
+
         if isNewPackage {
             currentPackageElement = XMLElement(name: "package")
             packagesElement.addChild(currentPackageElement)
         }
-        
+
         currentPackage = packageName
         if isNewPackage {
             currentPackageElement.addAttribute(XMLNode.attribute(withName: "name", stringValue: packageName) as! XMLNode)
@@ -125,7 +125,7 @@ func generateCoberturaReport(from coverageReport: CoverageReport, targetsToInclu
             currentPackageElement.addAttribute(XMLNode.attribute(withName: "branch-rate", stringValue: "1.0") as! XMLNode)
             currentPackageElement.addAttribute(XMLNode.attribute(withName: "complexity", stringValue: "0.0") as! XMLNode)
         }
-        
+
         let classElement = XMLElement(name: "class")
         classElement.addAttribute(XMLNode.attribute(withName: "name", stringValue: "\(packageName).\((fileCoverageReport.name as NSString).deletingPathExtension)") as! XMLNode)
         classElement.addAttribute(XMLNode.attribute(withName: "filename", stringValue: "\(filePath)") as! XMLNode)
@@ -133,32 +133,71 @@ func generateCoberturaReport(from coverageReport: CoverageReport, targetsToInclu
         classElement.addAttribute(XMLNode.attribute(withName: "branch-rate", stringValue: "1.0") as! XMLNode)
         classElement.addAttribute(XMLNode.attribute(withName: "complexity", stringValue: "0.0") as! XMLNode)
         currentPackageElement.addChild(classElement)
-        
+
         let linesElement = XMLElement(name: "lines")
         classElement.addChild(linesElement)
-        
+
+    /* Old Style
         for functionCoverageReport in fileCoverageReport.functions {
             for index in 0..<functionCoverageReport.executableLines {
-                // Function coverage report won't be 100% reliable without parsing it by file (would need to use xccov view --file filePath workingDirectory + Build/Logs/Test/*.xccovarchive)
+
+                // Function coverage report won't be 100% reliable without parsing it by file (would need to use xccov view --file filePath workingDirectory + Build/Logs/Test/ *.xccovarchive)
                 let lineElement = XMLElement(kind: .element, options: .nodeCompactEmptyElement)
                 lineElement.name = "line"
                 lineElement.addAttribute(XMLNode.attribute(withName: "number", stringValue: "\(functionCoverageReport.lineNumber + index)") as! XMLNode)
                 lineElement.addAttribute(XMLNode.attribute(withName: "branch", stringValue: "false") as! XMLNode)
-                
+
                 let lineHits: Int
                 if index < functionCoverageReport.coveredLines {
                     lineHits = functionCoverageReport.executionCount
                 } else {
                     lineHits = 0
                 }
-                
+
                 lineElement.addAttribute(XMLNode.attribute(withName: "hits", stringValue: "\(lineHits)") as! XMLNode)
                 linesElement.addChild(lineElement)
             }
+        } */
+
+        /* Ci sono degli errori nei dati di input: executableLines è sbagliato; da il numero di linee coperte, ma non dice quali, ecc.*/
+        /* Consideara le classi coincidenti con i file per cui parso l'intero file */
+
+        let covFile = fileCoverageReport.name.replacingOccurrences(of: ".swift", with: ".cov")
+        guard let covData = try? String(contentsOfFile: (covPath + covFile)) else {
+            return "COV!!"
+        }
+        let covLines = covData.components(separatedBy: .newlines)
+        linesElement.addAttribute(XMLNode.attribute(withName: "fileName", stringValue: "\(covFile)") as! XMLNode)
+
+        for index in 0..<covLines.count - 1 {
+            guard let line = getLine(id: index, lines: covLines), !line.hasSuffix("*") else {
+                continue
+            }
+
+            let lineElement = XMLElement(kind: .element, options: .nodeCompactEmptyElement)
+            lineElement.name = "line"
+            lineElement.addAttribute(XMLNode.attribute(withName: "number", stringValue: "\(index)") as! XMLNode)
+            lineElement.addAttribute(XMLNode.attribute(withName: "branch", stringValue: "false") as! XMLNode)
+
+            let components = line.components(separatedBy: " ")
+            let covHits = Int(components.last ?? "") ?? 0
+            lineElement.addAttribute(XMLNode.attribute(withName: "hits", stringValue: "\(covHits)") as! XMLNode)
+
+            linesElement.addChild(lineElement)
         }
     }
-    
+
     return doc.xmlString(options: [.nodePrettyPrint])
+}
+
+func getLine(id: Int, lines: [String]) -> String? {
+
+    for line in lines {
+        if line.trimmingCharacters(in: .whitespaces).starts(with: "\(id): ") {
+            return line
+        }
+    }
+    return nil
 }
 
 let arguments = Array(ProcessInfo.processInfo.arguments.dropFirst())
@@ -169,10 +208,17 @@ let workingDirectoryArg = parser.add(option: "--working-directory", shortName: "
 let targetsToIncludeArg = parser.add(option: "--targetsToInclude", shortName: "-t", kind: [String].self, usage: "List of targets to include in the coverage report")
 let packagesToExcludeArg = parser.add(option: "--packagesToExclude", shortName: "-p", kind: [String].self, usage: "List of packages to exclude from the coverage report")
 
+let covPathArg = parser.add(option: "--linesCoverage", shortName: "-c", kind: String.self, usage: "Path of detailed coverage files")     // "linescov/"
+
 let parsedArguments = try parser.parse(arguments)
 
 guard let jsonReportPath = parsedArguments.get(jsonReportPathArg) else {
     print("Missing JSON xccov report path")
+    exit(1)
+}
+
+guard let covPath = parsedArguments.get(covPathArg) else {
+    print("Missing detailed coverage files path")
     exit(1)
 }
 
@@ -192,5 +238,5 @@ guard let report = try? JSONDecoder().decode(CoverageReport.self, from: data) el
 
 let targetsToInclude = parsedArguments.get(targetsToIncludeArg) ?? []
 let packagesToExclude = parsedArguments.get(packagesToExcludeArg) ?? []
-let coberturaReport = generateCoberturaReport(from: report, targetsToInclude: targetsToInclude, packagesToExclude: packagesToExclude, workingDirectory: workingDirectory)
+let coberturaReport = generateCoberturaReport(from: report, targetsToInclude: targetsToInclude, packagesToExclude: packagesToExclude, workingDirectory: workingDirectory, covPath: covPath)
 print("\(coberturaReport)")
