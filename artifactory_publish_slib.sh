ce144fa38c90bfe5e5022dfe48475867c302bc9a
Enrico Bonaldo <asclepix@yahoo.it>
2020-05-05 16:35:17 +0200

    Build and deploy static library

---
+++ artifactory_publish_slib.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+# TODO leggere $ARCHIVE_PATH da Xcode ??
+
+echo
+echo "Archivia su Artifactory il risultato di un archive di ALCipher-Universal"
+echo "Chiamare dalla cartella contenente ALChiper.xcodeproj"
+echo
+
+
+require_property() {
+  PROPERTY=$(cat "$1" | grep $2 | cut -d= -f2)
+  if [ -z "$PROPERTY" ]; then
+    echo "Property $2 not found" >&2
+    exit 1
+  fi
+  echo $PROPERTY
+}
+
+require_gradle_property() {
+	echo $(require_property ~/.gradle/gradle.properties $1)
+}
+
+get_current_index_json() {
+  SEPARATOR="RET_CODE"
+  CURL_OUTPUT=$(curl -u$1:$2 -s -w "\n$SEPARATOR%{http_code}\n" "$3") || exit $?
+  RETURN_CODE="${CURL_OUTPUT##*$SEPARATOR}"
+  case $RETURN_CODE in
+    "200") DOWNLOADED_JSON="${CURL_OUTPUT%$SEPARATOR*}"; echo $DOWNLOADED_JSON;;
+    "404") echo "{}";;
+    *) exit 1
+  esac
+}
+
+increment_ver() {
+    version="$1"
+    major=0
+    minor=0
+    build=0
+    
+    # break down the version number into it's components
+    regex="([0-9]+).([0-9]+).([0-9]+)"
+    if [[ $version =~ $regex ]]; then
+      major="${BASH_REMATCH[1]}"
+      minor="${BASH_REMATCH[2]}"
+      build="${BASH_REMATCH[3]}"
+    fi
+    build=$(echo $build + 1 | bc)
+    updated_version=${major}.${minor}.${build}
+    echo $updated_version
+}
+
+# -----------------------------------------------------
+# MAIN
+# -----------------------------------------------------
+
+ARTIFACTORY_USER=$(require_gradle_property "artifactoryUser") || exit $?
+ARTIFACTORY_PASSWORD=$(require_gradle_property "artifactoryPassword") || exit $?
+echo "Artifactory credentials retrieved successfully"
+
+ARTIFACT_URL="https://artifactory.aliaslab.net/artifactory/SecureCallOTP_libCipher_iOS_Release"
+JSON_URL=$ARTIFACT_URL/libALCipher.json
+VARIANT="Release-universal"
+PRODUCT="libALChiper.a.zip"
+
+echo "Downloading JSON from Artifactory"
+ORIGINAL_INDEX_JSON=$(get_current_index_json "$ARTIFACTORY_USER" "$ARTIFACTORY_PASSWORD" "$JSON_URL") || exit $?
+#echo $ORIGINAL_INDEX_JSON > orig.json
+ORIGINAL_VER=$(echo $ORIGINAL_INDEX_JSON | jq 'keys[-1]')
+#echo $ORIGINAL_VER
+VER=$(increment_ver $ORIGINAL_VER)
+echo $VER
+
+UPDATED_JSON=$(echo $ORIGINAL_INDEX_JSON | jq --arg "artifactURL" "$ARTIFACT_URL/$VER/$PRODUCT" '. + {"'$VER'": $artifactURL}') || exit $?
+echo "Updated JSON is $UPDATED_JSON"
+
+exit
+
+BUILD_DIR=$(xcodebuild -project ALChiper.xcodeproj -target "ALCipher-Universal" -showBuildSettings | grep -w BUILD_DIR | cut -d= -f2)
+
+# lo trovo nelle var di Xcode?
+ARCHIVE_PATH="Intermediates.noindex/ArchiveIntermediates/ALCipher-Universal/BuildProductsPath"
+
+#TEMP_DIR="$PWD"
+cd ${BUILD_DIR}
+cd ..
+cd "${ARCHIVE_PATH}/${VARIANT}"
+if [ -f $PRODUCT ]; then
+	rm $PRODUCT
+fi
+zip -r $PRODUCT *
+
+ARTIFACT_MD5_CHECKSUM=$(md5 -q "$PRODUCT")
+ARTIFACT_SHA1_CHECKSUM=$(shasum -a 1 "$PRODUCT" | awk '{ print $1 }')
+
+echo "Uploading framework to Artifactory"
+curl -u$ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD -T $PRODUCT --header "X-Checksum-MD5:${ARTIFACT_MD5_CHECKSUM}" --header "X-Checksum-Sha1:${ARTIFACT_SHA1_CHECKSUM}" "$ARTIFACT_URL/$VER/$PRODUCT"
+
+echo "Uploading JSON to Artifactory"
+JSON_MD5_CHECKSUM=$(echo $UPDATED_JSON | md5 -q)
+JSON_SHA1_CHECKSUM=$(echo $UPDATED_JSON | shasum -a 1 | awk '{ print $1 }')
+echo $UPDATED_JSON | curl -u$ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD -T - --header "X-Checksum-MD5:${JSON_MD5_CHECKSUM}" --header "X-Checksum-Sha1:${JSON_SHA1_CHECKSUM}" "$JSON_URL"
+
+#cd "$TEMP_DIR"
---
+++ make_static.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+CONFIGURATION="Release"
+TARGET="ALCipher-Universal"
+PROJECT="ALChiper.xcodeproj"
+BUILD_DIR=$(xcodebuild -project $PROJECT -target "$TARGET" -showBuildSettings | grep -w BUILD_DIR | cut -d= -f2)
+BUILD_ROOT=$(xcodebuild -project $PROJECT -target "$TARGET" -showBuildSettings | grep -w BUILD_ROOT | cut -d= -f2)
+PROJECT_NAME=$(xcodebuild -project $PROJECT -target "$TARGET" -showBuildSettings | grep -w PROJECT_NAME | awk '{print $3}')
+echo "Building $PROJECT_NAME..."
+
+# define output folder environment variable
+UNIVERSAL_OUTPUTFOLDER=${BUILD_DIR}/${CONFIGURATION}-universal
+
+# Step 1. Build Device and Simulator versions
+# OTHER_CFLAGS="-fembed-bitcode" force BITCODE even in debug
+xcodebuild OTHER_CFLAGS="-fembed-bitcode" -target ALChiper ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphoneos BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
+xcodebuild OTHER_CFLAGS="-fembed-bitcode" -target ALChiper -configuration ${CONFIGURATION} -sdk iphonesimulator BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}"
+
+# make sure the output directory exists
+mkdir -p "${UNIVERSAL_OUTPUTFOLDER}"
+
+# Step 2. Create universal binary file using lipo
+lipo -create -output "${UNIVERSAL_OUTPUTFOLDER}/lib${PROJECT_NAME}.a" "${BUILD_DIR}/${CONFIGURATION}-iphoneos/lib${PROJECT_NAME}.a" "${BUILD_DIR}/${CONFIGURATION}-iphonesimulator/lib${PROJECT_NAME}.a"
+
+# Last touch. copy the header files. Just for convenience
+cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/include" "${UNIVERSAL_OUTPUTFOLDER}/"
+
+echo "------------------------------------------------"
+STATIC_LIB="${UNIVERSAL_OUTPUTFOLDER}/lib${PROJECT_NAME}.a"
+#lipo -info "${STATIC_LIB}"
+#otool -l "${STATIC_LIB}" | grep __LLVM > /dev/null
+
+echo "Static library: ${STATIC_LIB}"
+ARCHITECTURES=$(lipo -archs "${STATIC_LIB}")
+echo -e "Architectures:\t  $ARCHITECTURES"
+echo
+
+for ARCH in $ARCHITECTURES
+do
+	otool -l -arch $ARCH "${STATIC_LIB}" | grep __LLVM > /dev/null
+	if [ $? -eq 0 ]; then
+		echo -e "\t[*] $ARCH has BITCODE"
+	else
+		echo -e "\t[X] $ARCH has not BITCODE"
+	fi
+done
+echo "------------------------------------------------"
+
+#open "${UNIVERSAL_OUTPUTFOLDER}/"
