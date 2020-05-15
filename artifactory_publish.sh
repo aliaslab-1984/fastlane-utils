8c9750af5a56311ae9856a5e5bdbf09054bc6240
Enrico Bonaldo <asclepix@yahoo.it>
2020-05-15 09:31:18 +0200

    Added xcframework deploy

--- artifactory_publish.sh
+++ artifactory_publish.sh
@@ -25,6 +25,14 @@ get_build_target_type() {
   esac
 }
 
+get_xc_version_string() {
+    PLIST_JSON=$(cat "$1/Info.plist" | plutil -convert json - -o -) || exit $?
+    SUB=$(echo $PLIST_JSON | jq '.AvailableLibraries[1].LibraryIdentifier' | sed -e 's/^"//' -e 's/"$//')
+    SUBPATH="$1/$SUB/${FRAMEWORK_NAME}.framework"
+    BUNDLE_VERSION=$(get_version_string "${SUBPATH}")
+    echo $BUNDLE_VERSION
+}
+
 get_version_string() {
   PLIST_JSON=$(cat "$1/Info.plist" | plutil -convert json - -o -) || exit $?
   BUNDLE_SHORT_VERSION_STRING=$(echo $PLIST_JSON | jq '.CFBundleShortVersionString' | sed -e 's/^"//' -e 's/"$//')
@@ -59,7 +67,7 @@ zip_framework() {
 
   rm -f "$1"
   cd "$2"
-  zip -r "$1" "$3.framework"
+  zip -r "$1" "$3" -x \*.DS_Store
   RESULT=$?
   cd "$OLDPWD"
   return $RESULT
@@ -78,13 +86,18 @@ check_artifactory_response() {
   fi
 }
 
-while getopts ":c:f:" opt; do
+XC_PREFIX=""
+
+while getopts ":c:f:x" opt; do
   case $opt in
     c)
       CONFIG_FILE_PATH=$OPTARG
       ;;
     f)
       FRAMEWORK_PATH=$OPTARG
+      ;;
+    x)
+      XC_PREFIX="xc"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -114,21 +127,28 @@ ARTIFACTORY_PASSWORD=$(require_gradle_property "artifactoryPassword") || exit $?
 echo "Artifactory credentials retrieved successfully"
 
 FRAMEWORK_NAME=$(require_property "$CONFIG_FILE_PATH" "frameworkName") || exit $?
-FRAMEWORK_FILE=$FRAMEWORK_PATH/$FRAMEWORK_NAME.framework
-ZIPPED_FRAMEWORK_FILE="$(pwd)/$FRAMEWORK_NAME.framework.zip"
+FRAMEWORK_FILE="$FRAMEWORK_PATH/${FRAMEWORK_NAME}.${XC_PREFIX}framework"
+ZIPPED_FRAMEWORK_FILE="$(pwd)/${FRAMEWORK_NAME}.${XC_PREFIX}framework.zip"
 echo "Zipping framework at $FRAMEWORK_FILE into $ZIPPED_FRAMEWORK_FILE"
-zip_framework "$ZIPPED_FRAMEWORK_FILE" "$FRAMEWORK_PATH" "$FRAMEWORK_NAME" || exit $?
+zip_framework "$ZIPPED_FRAMEWORK_FILE" "$FRAMEWORK_PATH" "$FRAMEWORK_NAME.${XC_PREFIX}framework" || exit $?
 
-TARGET_TYPE=$(get_build_target_type "$FRAMEWORK_FILE/$FRAMEWORK_NAME") || exit $?
+if [ -z "$XC_PREFIX" ]; then
+    TARGET_TYPE=$(get_build_target_type "$FRAMEWORK_FILE/$FRAMEWORK_NAME") || exit $?
+    VERSION_STRING=$(get_version_string "$FRAMEWORK_FILE") || exit $?
+else
+    TARGET_TYPE="xcframework"
+    VERSION_STRING=$(get_xc_version_string "$FRAMEWORK_FILE") || exit $?
+fi
+echo "================================================================="
+echo "Zipped file is $ZIPPED_FRAMEWORK_FILE"
 echo "Target is of type $TARGET_TYPE"
-
-VERSION_STRING=$(get_version_string "$FRAMEWORK_FILE") || exit $?
 echo "Version string is $VERSION_STRING"
 
 REPOSITORY_NAME=$(get_repository_name "$CONFIG_FILE_PATH" $VERSION_STRING) || exit $?
+#REPOSITORY_NAME="IDSignMobileStandaloneSDK_iOS_Snapshot"
 echo "Repository name is $REPOSITORY_NAME"
 
-ARTIFACT_URL=$ARTIFACTORY_URL/$REPOSITORY_NAME/$TARGET_TYPE/$VERSION_STRING/$FRAMEWORK_NAME.framework.zip
+ARTIFACT_URL=$ARTIFACTORY_URL/$REPOSITORY_NAME/$TARGET_TYPE/$VERSION_STRING/${FRAMEWORK_NAME}.${XC_PREFIX}framework.zip
 JSON_URL=$ARTIFACTORY_URL/$REPOSITORY_NAME/$TARGET_TYPE/$FRAMEWORK_NAME.json
 
 ORIGINAL_INDEX_JSON=$(get_current_index_json "$ARTIFACTORY_USER" "$ARTIFACTORY_PASSWORD" "$JSON_URL") || exit $?
@@ -138,11 +158,12 @@ UPDATED_JSON=$(echo $ORIGINAL_INDEX_JSON | jq --arg "artifactURL" "$ARTIFACT_URL
 echo "Updated JSON is $UPDATED_JSON"
 echo "Artifact URL is $ARTIFACT_URL"
 echo "JSON URL is $JSON_URL"
+echo "================================================================="
 
 echo "Uploading framework to Artifactory"
 ARTIFACT_MD5_CHECKSUM=$(md5 -q "$ZIPPED_FRAMEWORK_FILE")
 ARTIFACT_SHA1_CHECKSUM=$(shasum -a 1 "$ZIPPED_FRAMEWORK_FILE" | awk '{ print $1 }')
-CURL_OUTPUT=$(curl -u$ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD -T "$ZIPPED_FRAMEWORK_FILE" --header "X-Checksum-MD5:${ARTIFACT_MD5_CHECKSUM}" --header "X-Checksum-Sha1:${ARTIFACT_SHA1_CHECKSUM}" "$ARTIFACT_URL")
+CURL_OUTPUT=$(curl -u$ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD -T "${ZIPPED_FRAMEWORK_FILE}" --header "X-Checksum-MD5:${ARTIFACT_MD5_CHECKSUM}" --header "X-Checksum-Sha1:${ARTIFACT_SHA1_CHECKSUM}" "$ARTIFACT_URL")
 rm -f "$ZIPPED_FRAMEWORK_FILE"
 check_artifactory_response "$CURL_OUTPUT" || exit $?
 
